9a21da27b35f39097b90db00a25843357690df62
JuanasoKsKs <jesse.bmz16@gmail.com>
2026-01-05 21:41:03 -0700

    copy Static to Public Function

--- src/main.py
+++ src/main.py
@@ -1,13 +1,28 @@
-from textnode import TextNode, TextType
-from htmlnode import LeafNode, ParentNode, HTMLNode
+import os
+import shutil
+
 def main():
-    dummy = TextNode("Anchor text", TextType.LINK, "Juanaso.com/Sisiriski")
-    dummy2 = HTMLNode("p", "values", None, None)
-    dummy3 = LeafNode("p", "values", None)
-    dummy4 = ParentNode("p", [dummy2, dummy3], None)
-    print(dummy)
-    print(dummy2)
-    print(dummy3)
-    print(dummy4)
+    project_path = os.path.abspath("")
+    static_path = os.path.join(project_path, "static")
+    public_path = os.path.join(project_path, "public")
+    copy_to_dic(static_path, public_path)
+
+#this function overwrites/create the "dest" folder
+def copy_to_dic(origin, dest):
+    if os.path.exists(dest):
+        delete_dir(dest)
+    os.mkdir(dest)
+    items = os.listdir(origin)
+    for item in items:
+        item_path = os.path.join(origin, item)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, dest)
+        else:
+            dic_path = os.path.join(dest, item)
+            copy_to_dic(item_path, dic_path)
+    
+def delete_dir(path):
+    shutil.rmtree(path)
+
 
 main()
