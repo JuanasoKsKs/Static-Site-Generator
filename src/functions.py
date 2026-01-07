5db3b305be6e4d760031bc3076b2729281cfdc2c
JuanasoKsKs <jesse.bmz16@gmail.com>
2026-01-07 01:09:37 -0600

    generation of first page (Markdown to index.html)

--- src/functions.py
+++ src/functions.py
@@ -2,6 +2,7 @@ from textnode import TextNode, TextType, text_node_to_html_node
 import re
 from blocktype import block_to_block_type, BlockType
 from htmlnode import LeafNode, ParentNode
+import os
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -101,7 +102,6 @@ def markdown_to_html_node(markdown):
         first_borns.extend(childrens_html)
     return ParentNode("div", first_borns)
 
-    
 def text_to_children(text, type_of_block):
     nodes = []
     if type_of_block == BlockType.HEADING:
@@ -115,7 +115,11 @@ def text_to_children(text, type_of_block):
         parent_node = ParentNode(type_of_block.value, [])
         for line in lines:
             divided_line = line.split(" ", 1)
-            parent_node.children.append(LeafNode("li", divided_line[1]))
+            text_nodes = text_to_textnodes(divided_line[1])
+            childrens_in_leaf = []
+            for text_node in text_nodes:
+                childrens_in_leaf.append(text_node_to_html_node(text_node))
+            nodes.append(ParentNode("li", childrens_in_leaf))            
         nodes.append(parent_node)
 
     elif type_of_block == BlockType.QUOTE:
@@ -132,7 +136,24 @@ def text_to_children(text, type_of_block):
         nodes.append(ParentNode("p", childrens_in_leaf))
     return nodes
     
-
-
-          
-
+def extract_title(markdown):
+    markdown = markdown.split("\n\n")
+    for i in markdown:
+        if i.startswith("# "):
+            result = i[1:].strip(" ")
+            return result
+        
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as f:
+        markdown = f.read()
+    with open(template_path, "r") as f:
+        template = f.read()
+    content = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+    final_html = template.replace("{{ Title }}", title)
+    final_html = final_html.replace("{{ Content }}", content)
+    
+    with open(dest_path, "w") as f:
+        f.write(final_html)
+    print(final_html)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 import os
 import shutil
+from functions import extract_title, generate_page
 
 def main():
     project_path = os.path.abspath("")
     static_path = os.path.join(project_path, "static")
     public_path = os.path.join(project_path, "public")
+    content_path = os.path.join(project_path, "content/index.md")
+    template_path = os.path.join(project_path, "template.html")
+    dest_path = os.path.join(project_path, "public/index.html")
     copy_to_dic(static_path, public_path)
+    generate_page(content_path, template_path, dest_path)
 
 #this function overwrites/create the "dest" folder
 def copy_to_dic(origin, dest):
